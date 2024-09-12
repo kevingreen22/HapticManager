02621c206ceed09f264f7dfdc060d6bb79058954
Kevin Green <stuckbykev@gmail.com>
2024-09-12 12:59:15 -0700

    Changed package name.

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "Haptics",
+    name: "HapticManager",
     platforms: [
         .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Haptics",
-            targets: ["Haptics"]),
+            name: "HapticManager",
+            targets: ["HapticManager"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Haptics"),
+            name: "HapticManager"),
         .testTarget(
-            name: "HapticsTests",
-            dependencies: ["Haptics"]),
+            name: "HapticManagerTests",
+            dependencies: ["HapticManager"]),
     ]
 )
